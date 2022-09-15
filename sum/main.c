86ae61d01223c97b73eef71f66e68c134ee4c8cf
morteza <mortezashoeibi77@gmail.com>
2022-09-16 03:16:50 +0430

    variables in C

---
+++ sum/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int first_number = 222;
+    int second_number = 444;
+    int evil_number = first_number + second_number;
+    printf("The result is so Evil! it's: %d\n" ,evil_number);
+
+    return 0;
+}
