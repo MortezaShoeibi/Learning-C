77dcc0eecad1c332a356558384f6b87da6b543ef
morteza <mortezashoeibi77@gmail.com>
2022-09-18 03:50:11 +0430

    C operators

---
+++ Operators/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    C operators:
+        ()
+        / *
+        %
+        + -
+        =
+*/
+
+int main() {
+
+    int i;
+    i = 12 + 16 / 2 - 1 * (5 + 6);
+    printf("%d\n", i);
+    
+    return 0;
+}
