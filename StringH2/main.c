26c1c5ff80693556e730d02f0e2eb9ccf3c227f0
morteza <mortezashoeibi77@gmail.com>
2022-10-06 16:19:52 +0330

    string.h part2

---
+++ StringH2/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STR_LEN 50
+
+int main()
+{
+    char string1[MAX_STR_LEN];
+    char string2[MAX_STR_LEN];
+    strcpy(string1, "hello");
+    strcpy(string2, "world");
+
+    printf("string1: %s\t string2: %s\n", string1, string2);
+    printf("%s, %s!\n", string1, string2);
+
+    puts("------------------------------");
+
+    strcat(string1, string2);
+    printf("string1: %s\t string2: %s\n", string1, string2);
+
+    puts("------------------------------");
+
+    /*
+    NOTE: you can't compare strings valued variables
+        because they are pointers, to compare to strings
+        you have to user strcmp(); function.
+        if the tow variables were equal it'll return 0. 
+    */
+
+    if (strcmp(string1, string2) == 0)
+    {
+        puts("the strings are the same");
+    }
+    else
+    {   
+        puts("the strings are not the same");
+    }
+    
+
+    return 0;
+}
