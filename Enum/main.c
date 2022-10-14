db58a7cf9ae75535d6ac3160f561aa70dcf0e0a5
morteza <mortezashoeibi77@gmail.com>
2022-10-14 15:08:10 +0330

    enum

---
+++ Enum/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum months
+{
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec,
+};
+
+int main()
+{
+    enum months m;
+    m = Jul;
+
+    return 0;
+}
